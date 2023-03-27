0c7d2d85ce9ef5bb4e7de8e5fa43e101b993aaf6
zslouma <s.zaalouni@archimed.fr>
2023-03-27 23:03:05 +0100

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Test_tech_MSZ.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
